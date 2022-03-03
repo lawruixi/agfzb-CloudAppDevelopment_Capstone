0f648128ab1d9c3ca207debe4c713ac045ced8cd
evelene law <lawruixi@gmail.com>
2022-03-03 05:38:28 +0000

    added redirect from / and updated titles and navbars for all pages

--- server/djangobackend/urls.py
+++ server/djangobackend/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
+    path('', RedirectView.as_view(pattern_name="djangoapp:index"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
